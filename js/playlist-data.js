68848a3455411928864feb3d62f2b6572e6e995d
Sufian <133886691+SufianMakkad@users.noreply.github.com>
2026-08-22 20:48:14 +0100

    Update playlist-data.js

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -1,12 +1,9 @@
 /* How to add files:
-
 Add title, then artist, in the same formatting, for each song.
 */
-
 window.SONGS = [
-  { title: "Lilies", artist: "Ethel Cain, Mercy Necromancy", data: "https://github.com/SufianMakkad/for-dena/blob/main/assets/audio/Lilies.mp3?raw=true" },
-  { title: "Ripples of Past Reverie", artist: "HOYO-MiX, Cassie Wei", data: "blob/main/assets/audio/Ripples%20of%20Past%20Reverie.mp3" },
-  { title: "Carousel", artist: "Laufey", data: "blob/main/assets/audio/Carousel" },
-  { title: "favorite apple", artist: "The Two Lips", data: "blob/main/assets/audio/favorite%20apple.mp3" },
+  { title: "Lilies", artist: "Ethel Cain, Mercy Necromancy", data: "assets/audio/Lilies.mp3" },
+  { title: "Ripples of Past Reverie", artist: "HOYO-MiX, Cassie Wei", data: "assets/audio/Ripples%20of%20Past%20Reverie.mp3" },
+  { title: "Carousel", artist: "Laufey", data: "assets/audio/Carousel.mp3" },
+  { title: "favorite apple", artist: "The Two Lips", data: "assets/audio/favorite%20apple.mp3" },
 ];
-
