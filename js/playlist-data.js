43ea0065e45e2f485465b96dd6d136430f24b211
Sufian <133886691+SufianMakkad@users.noreply.github.com>
2026-08-22 20:19:49 +0100

    Update playlist-data.js

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -4,9 +4,9 @@ Add title, then artist, in the same formatting, for each song.
 */
 
 window.SONGS = [
-  { title: "Lilies", artist: "Ethel Cain, Mercy Necromancy", data: "for-dena/js/assets/audio/Lilies.mp3" },
-  { title: "Ripples of Past Reverie", artist: "HOYO-MiX, Cassie Wei", data: "for-dena/js/assets/audio/Ripples%20of%20Past%20Reverie.mp3" },
-  { title: "Carousel", artist: "Laufey", data: "for-dena/js/assets/audio/Carousel.mp3" },
-  { title: "favorite apple", artist: "The Two Lips", data: "for-dena/js/assets/audio/favorite%20apple.mp3" },
+  { title: "Lilies", artist: "Ethel Cain, Mercy Necromancy", data: "blob/main/assets/audio/Lilies.mp3" },
+  { title: "Ripples of Past Reverie", artist: "HOYO-MiX, Cassie Wei", data: "blob/main/assets/audio/Ripples%20of%20Past%20Reverie.mp3" },
+  { title: "Carousel", artist: "Laufey", data: "blob/main/assets/audio/Carousel.mp3" },
+  { title: "favorite apple", artist: "The Two Lips", data: "blob/main/assets/audio/favorite%20apple.mp3" },
 ];
 
