b7bc581af11f37729fd20dc380cef3ff9958d8d4
Sufian <133886691+SufianMakkad@users.noreply.github.com>
2026-08-22 21:11:03 +0100

    Update playlist-data.js

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -6,4 +6,5 @@ window.SONGS = [
   { title: "Ripples of Past Reverie", artist: "HOYO-MiX, Cassie Wei", data: "assets/audio/Ripples%20of%20Past%20Reverie.mp3" },
   { title: "Carousel", artist: "Laufey", data: "assets/audio/Carousel.mp3" },
   { title: "favorite apple", artist: "The Two Lips", data: "assets/audio/favorite%20apple.mp3" },
+  { title: "JANE DOE", artist: "Kenshi Yonezu, Hikaru Utada", data: "assets/audio/JANE%20DOE.mp3" },
 ];
