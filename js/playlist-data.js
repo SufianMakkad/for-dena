c1630093b65db7dd9329c681c8addfb2fa307bd2
Sufian <133886691+SufianMakkad@users.noreply.github.com>
2026-08-31 17:43:56 +0100

    Update playlist-data.js

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -1,6 +1,7 @@
 /* How to add files:
 Add title, then artist, in the same formatting, for each song.
 */
+
 window.SONGS = [
   { title: "favorite apple", artist: "The Two Lips", data: "assets/audio/favorite%20apple.mp3" },
   { title: "Lilies", artist: "Ethel Cain, Mercy Necromancy", data: "assets/audio/Lilies.mp3" },
@@ -8,4 +9,5 @@ window.SONGS = [
   { title: "Carousel", artist: "Laufey", data: "assets/audio/Carousel.mp3" },
   { title: "JANE DOE", artist: "Kenshi Yonezu, Hikaru Utada", data: "assets/audio/JANE%20DOE.mp3" },
   { title: "Love song", artist: "Lana Del Ray", data: "assets/audio/Love%20song.mp3" },
+  { title: "Diet Pepsi", arsit: "Addison Rae", data: "assets/audio/Diet%20Pepsi.mp3" },
 ];
