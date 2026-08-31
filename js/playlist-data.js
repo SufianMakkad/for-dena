cf61d58ef564b08c7dd59c50ff5766988c701232
Sufian <133886691+SufianMakkad@users.noreply.github.com>
2026-08-31 17:49:25 +0100

    Update playlist-data.js

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -9,5 +9,7 @@ window.SONGS = [
   { title: "Carousel", artist: "Laufey", data: "assets/audio/Carousel.mp3" },
   { title: "JANE DOE", artist: "Kenshi Yonezu, Hikaru Utada", data: "assets/audio/JANE%20DOE.mp3" },
   { title: "Love song", artist: "Lana Del Ray", data: "assets/audio/Love%20song.mp3" },
-  { title: "Diet Pepsi", arsit: "Addison Rae", data: "assets/audio/Diet%20Pepsi.mp3" },
+  { title: "Diet Pepsi", artist: "Addison Rae", data: "assets/audio/Diet%20Pepsi.mp3" },
+  { title: "El Hob Gany | الحب جاني", artist: "TUL8TE", data: "assets/audio/El%20Hob%Gany.mp3" },
+  { title: "KALAMANTINA | كلمنتينا" , artist: "Saint Levant ft. Marwan Moussa", data: "assets/audio/Kalamantina.mp3" },
 ];
